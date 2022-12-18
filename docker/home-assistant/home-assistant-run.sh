a707eb543038342b3f1f46e8262c0311402e7ac4
MJendza <mateusz.jendza@gmail.com>
2022-12-18 09:55:07 +0000

    fixed coping HA config to runtime

--- docker/home-assistant/home-assistant-run.sh
+++ docker/home-assistant/home-assistant-run.sh
@@ -4,6 +4,6 @@ mkdir -p ../../dist
 mkdir -p ../../dist/ha
 mkdir -p ../../dist/ha/config
 
-cp -a ../../home-assistant-configuration/ ../../dist/ha/config/
+cp -a ../../home-assistant-configuration/. ../../dist/ha/config/
 
 docker-compose -f docker-compose.yaml -f docker-compose.dev.override.yaml --env-file .env.dev up --detach --force-recreate
